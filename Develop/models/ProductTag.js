cd9be658540b0b74659b8b277960ee1e53a594ff
Max <max.belenchuk@gmail.com>
2023-02-06 23:34:01 -0400

    tag id requirments defined

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -18,8 +18,16 @@ ProductTag.init(
       references: {
         model: "product",
         key: "id"
-      }
-    }
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      },
+    },
+    
     // define columns
   },
   {
